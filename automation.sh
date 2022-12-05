e3d5a48ff07d534dcc040a257dc6e6e6a483f3a4
Sujata Ganguly <88281618+Shrijata07@users.noreply.github.com>
2022-12-06 00:02:22 +0530

    Updated book keeping script and cron job task

--- automation.sh
+++ automation.sh
@@ -36,3 +36,26 @@ sudo systemctl enable apache2
 sudo tar -cvf /tmp/"${name}-httpd-logs-${timestamp}.tar" /var/log/apache2/*.log
 # Upload to S3 bucket
 aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
+
+#script to produce an HTML file book keeping
+
+if [[ ! -f var/www/html/inventory.html ]];
+then
+        echo -e Log Type\ \Date Created\ \Type\ \Size > /var/www/html/inventory.html
+fi
+
+if [[ -f /var/www/html/inventory.html ]];
+
+then
+        size=$(du -h /tmp/"${name}-httpd-logs-${timestamp}.tar" | awk '{print $1}')
+
+        echo -e "httpd-logs\t- \t$(date '+%d%m%Y-%H%M%S')\t- \ttar\t -\t${size}" >> /var/www/html/inventory.html
+
+fi
+
+#Cron job automation
+
+if [[ ! -f /etc/cron.d/automation ]];
+then
+        echo "0 11 * * 1-5 root /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
+fi
