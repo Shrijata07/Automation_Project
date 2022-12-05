9938c759f1a2764d099503ac4810c3574665649e
Sujata Ganguly <88281618+Shrijata07@users.noreply.github.com>
2022-12-05 23:08:00 +0530

    Updated comments on the script

--- automation.sh
+++ automation.sh
@@ -2,13 +2,16 @@
 
 sudo apt update -y
 
+# check if apache 2 is avaialble
+
+s3_bucket="upgrad-sujata"
+name="sujata"
+timestamp=$(date '+%d%m%Y-%H%M%S')
+
 if dpkg -l | grep -i apache2
-
 then
-
         echo "Apache is available"
 else
-
         echo "Apache is unavailable"
 
         sudo apt install apache2
@@ -19,27 +22,19 @@ fi
 STARTAPACHE="systemctl start apache2"
 
 if systemctl status apache2 | grep -q inactive
-
 then
-
         echo "starting apache at $(date)"
-
         $STARTAPACHE
 else
-
         echo "apache is running at $(date)"
 fi
 
 sudo systemctl status apache2.service
-
 sudo systemctl enable apache2
 
-mkdir tmp
-cd tmp
+#create tar files from the logs
 
-s3_bucket="upgrad-sujata"
-name="sujata"
+sudo tar -cvf /tmp/"${name}-httpd-logs-${timestamp}.tar" /var/log/apache2/*.log
+#copy to S3 bucket
 
-sudo tar -cvf "${name}-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar" /var/log/apache2/*.log
-
-aws s3 cp /root/Automation_Project/tmp/${name}-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar s3://${s3_bucket}/${name}-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar
+aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
